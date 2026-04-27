e67dad63bee01bf2fb8a392537d913a0783726c2
Akene Uzezi <xtremejoexj@gmail.com>
2026-04-27 13:31:12 +0100

    added a hero section from launch ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/sections/navbar/default";
+import Hero from "@/components/sections/hero/default";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
